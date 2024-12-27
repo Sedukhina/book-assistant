8fcca7951ba3d4121b1bec37b357eb3118518578
Viktor <52759202+Maho1000@users.noreply.github.com>
2024-12-27 02:43:19 +0200

    Update 3dModel.js

--- app/static/js/3dModel.js
+++ app/static/js/3dModel.js
@@ -1,33 +1,35 @@
 export const modelViewer = document.querySelector('#myModel');
 
 modelViewer.addEventListener('load', () => {
-  modelViewer.timeScale = 0.5; // Adjust this value to control speed
+    modelViewer.timeScale = 0.5; // Adjust this value to control speed
+    modelViewer.animationName = 'Idle_A'; // Set default animation
+    console.log('Model loaded, animation set to Idle_A');
 });
 
 
-export const ANIMATIONS = [
-  'Bounce',
-  'Eat',
-  'Idle_A',
-  'Roll',
-  'Run',
-  'Sit',
-  'Spin',
-  'Walk'
-]
+// export const ANIMATIONS = [
+//   'Bounce',
+//   'Eat',
+//   'Idle_A',
+//   'Roll',
+//   'Run',
+//   'Sit',
+//   'Spin',
+//   'Walk'
+// ]
 
 export const setAnimation = (animation) => {
   modelViewer.animationName = animation;
 }
 
 export const listenAnimation = () => {
-  modelViewer.animationName = 'Idle_A';
+  modelViewer.animationName = 'Spin';
 }
 
 export const waitAnimation = () => {
-  modelViewer.animationName = 'Eat';
+  modelViewer.animationName = 'Idle_A';
 }
 
 export const speakingAnimation = () => {
-  modelViewer.animationName = 'Run';
+  modelViewer.animationName = 'Eat';
 }
