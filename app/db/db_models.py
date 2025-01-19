6a2e3a7ff77c992940bf59cfc756c00c45d38cc3
Dmytro <just.another.dev.acc@gmail.com>
2025-01-19 02:43:27 +0200

    .

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,10 +1,13 @@
-import json
-from textwrap import indent
-
 from sqlalchemy import Column, Integer, String, Text, Date, Numeric, ForeignKey, TIMESTAMP, Table
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
+
+class BookCategoryBase(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
 
 class BookBase(Base):
     __tablename__ = 'books'
@@ -15,7 +18,7 @@ class BookBase(Base):
     author_id = Column(Integer, ForeignKey('authors.author_id'))
     publisher_id = Column(Integer, ForeignKey('publishers.publisher_id'))
     published_date = Column(Date)
-    category = Column(Text)
+    category_id = Column(Integer, ForeignKey('categories.id'))
     cover = Column(Text)
     ratings_count = Column(Numeric)
 
@@ -42,21 +45,9 @@ class BookBase(Base):
             "author_id":self.author.author_id if self.author else None,
             "author": self.author.name if self.author else None,
             "publishDate": int(self.published_date.year) if self.published_date else None,
-            "categories": self.category.strip().split('&') if self.category else [],
+            "categories": self.category_id if self.category_id else -1,
             "cover": self.cover,
         }
-    def to_json(self):
-        return json.dumps({
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "author": self.author.name if self.author else None,
-            "publisher_id": self.publisher_id,
-            "published_date": self.published_date.isoformat() if self.published_date else None,
-            "category": self.category,
-            "cover": self.cover,
-            "ratings_count": float(self.ratings_count) if self.ratings_count else None,
-        },indent=4)
 
 class AuthorBase(Base):
     __tablename__ = 'authors'
@@ -72,12 +63,6 @@ class AuthorBase(Base):
             "name": self.name,
             "books": [book.id for book in self.books] if self.books else []
         }
-
-# Table for author/book relashionships normalization
-book_author_association = Table('book_author', Base.metadata, 
-                                Column('book_id', Text, ForeignKey('books.id'), primary_key=True), 
-                                Column('author_id', Integer, ForeignKey('authors.author_id'), primary_key=True)
-)
 
 class PublisherBase(Base):
     __tablename__ = 'publishers'
@@ -104,10 +89,15 @@ class AmazonReviewBase(Base):
 class UserBase(Base):
     __tablename__ = 'users'
 
-    user_ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     email = Column(String(254), nullable=False, unique=True)
     username = Column(String(255), nullable=False)
     birthday = Column(Date, nullable=False)
     hashed_password = Column(String(300), nullable=False)
 
 
+class UserFavourites(Base):
+    __tablename__ = 'favourites'
+
+    book_id = Column(Text, ForeignKey('books.id'), primary_key=True) 
+    user_id = Column(Integer, ForeignKey('users.user_id'), primary_key=True)
